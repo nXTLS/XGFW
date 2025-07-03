8d92c1ef87be581cbc2eebe25176758c326bc942
NewTLS <zyk@jlhs.me>
2025-07-03 10:35:09 +0800

    feat: Add SOCKSModifier

--- cmd/root.go
+++ cmd/root.go
@@ -123,6 +123,7 @@ var modifiers = []modifier.Modifier{
 	&modTCP.HTTPModifier{},
 	&modTCP.TLSModifier{},
 	&modTCP.SSHModifier{},
+	&modTCP.SocksModifier{},
 	&modUDP.DNSModifier{},
 	&modUDP.CompressModifier{},
 	&modUDP.DynamicRouteModifier{},
